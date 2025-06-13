9214b7c987baab8984d474bb118e77f3e60abc59
MinDev <142654773+kminchk@users.noreply.github.com>
2025-06-13 13:09:36 +0700

    Initial Commit

---
+++ SkyNow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkyNow
+//
+//  Created by MINDEV on 13/6/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkyNow/SkyNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyNowApp.swift
+//  SkyNow
+//
+//  Created by MINDEV on 13/6/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct SkyNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
